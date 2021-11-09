4951c62e69c46ef333b2b30316f9dfd3f311643b
Esa <esakesti@gmail.com>
2021-11-09 00:55:06 +0000

    VirtualSocket created

--- src/VirtualSocket.java
+++ src/VirtualSocket.java
@@ -5,14 +5,24 @@ import java.util.Random;
 public class VirtualSocket extends DatagramSocket {
     private static double p_drop = 0.5;
 
-    public VirtualSocket() throws SocketException{
+    public VirtualSocket() throws SocketException {
         super();
     }
-    public VirtualSocket(int portti) throws SocketException{
+
+    public VirtualSocket(int portti) throws SocketException {
         super(portti);
 
     }
-    public void receive (DatagramPacket packet) throws IOException{
-        super.receive(packet);
+
+    public void receive(DatagramPacket packet) throws IOException {
+        while (true) {
+            Random randGen = new Random();
+            super.receive(packet);
+            if (randGen.nextDouble() <= p_drop) {
+                System.out.println("Dropper packet");
+            } else {
+                return;
+            }
+        }
     }
 }
