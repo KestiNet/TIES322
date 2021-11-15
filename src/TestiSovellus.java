8e8dab5a80c25ba1c1f56f1eb8277f0e39090593
Esa <esakesti@gmail.com>
2021-11-15 23:09:54 +0000

    Encoding issues fixed, now printing normally

--- src/TestiSovellus.java
+++ src/TestiSovellus.java
@@ -5,15 +5,16 @@ public class TestiSovellus {
     private static DatagramSocket soketti = null;
 
     public static void main(String[] args) throws Exception {
-        soketti  = new DatagramSocket(6666);
+        soketti  = new VirtualSocket(6666);
         boolean listening = true;
         while(listening){
             try {
                 byte[] rec = new byte[256];
                 DatagramPacket paketti = new DatagramPacket(rec, rec.length);
                 soketti.receive(paketti);
-                String str = new String(paketti.getData(), paketti.getLength());
-                System.out.println(str);
+               // String str =  // remove CRC8
+                System.out.println(new String(rec, 0, paketti.getLength()-1));
+                System.out.println(rec[paketti.getLength()-1]);
             }
             catch(Exception e){
                 listening = false;
