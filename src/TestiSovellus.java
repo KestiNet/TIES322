043c4e47f20dd2e7abb9724fd6ec4bac3b8d01a0
Esa <esakesti@gmail.com>
2021-11-21 00:43:32 +0000

    Uploading data to the server works now

--- src/TestiSovellus.java
+++ src/TestiSovellus.java
@@ -1,48 +1,36 @@
 import java.io.*;
 import java.net.*;
 import java.net.Socket.*;
+import java.nio.charset.StandardCharsets;
 
 public class TestiSovellus {
-    private static DatagramSocket soketti = null;
-    private static DatagramSocket ulkoSoketti = null;
+  //**  private static DatagramSocket soketti = null;
+    //private static DatagramSocket ulkoSoketti = null;
 
     public static void main(String[] args) throws Exception {
-        soketti  = new VirtualSocket(6666);
-        boolean listening = true;
-        while(listening){
-            try {
-                byte[] rec = new byte[256];
-                DatagramPacket paketti = new DatagramPacket(rec, rec.length);
-                soketti.receive(paketti);
-               // String str =  // remove CRC8
-                System.out.println(new String(rec, 0, paketti.getLength()-1));
-                System.out.println(rec[paketti.getLength()-1]);
-            }
-            catch(Exception e){
-                listening = false;
-                System.out.println("catcheed");
-                break;
-            }
-        }
-        Socket socket = new Socket("localhost", 54331);
-        System.out.println("Connected!");
 
-        // get the output stream from the socket.
-        OutputStream outputStream = socket.getOutputStream();
-        // create a data output stream from the output stream so we can send data through it
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+    /**    ulkoSoketti = new VirtualSocket(54331);
 
-        System.out.println("Sending string to the ServerSocket");
-
-        // write the message we want to send
-        dataOutputStream.writeUTF("Hello from the other side!");
+        DatagramPacket dataOutputStream = new DatagramPacket();
+        String str = "Moi";
+        DatagramPacket dp = new DatagramPacket(str.getBytes(),str.length(),ulkoSoketti);
+        dataOutputStream.("Hello from the other side!");
         dataOutputStream.flush(); // send the message
         dataOutputStream.close(); // close the output stream when we're done.
 
         System.out.println("Closing socket and terminating program.");
         socket.close();
+**/
 
+            DatagramSocket ds = new DatagramSocket();
+            String str = "Welcome java";
+            InetAddress ip = InetAddress.getByName("127.0.0.1");
 
+            DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ip, 54331);
+            ds.send(dp);
+            ds.close();
+        }
     }
-}
+
+
 
