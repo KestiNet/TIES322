6724ba19cfbb41e903b444637fe516c3c24d41dd
Esa <esakesti@gmail.com>
2021-11-22 01:09:31 +0000

    Started coding the client software to send data out with virtualsocket, need to learn more about this

---
+++ src/Client.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.*;
+import java.net.Socket.*;
+import java.nio.charset.StandardCharsets;
+
+public class Client {
+
+
+    private static DatagramSocket soketti = null;
+
+    public static void main(String[] args) throws Exception {
+        soketti = new VirtualSocket(55105);
+        boolean listening = true;
+        while (listening) {
+            try {
+                byte[] rec = new byte[256];
+                String str = "Welcome java ";
+
+                //DatagramPacket paketti = new DatagramPacket(rec, rec.length);
+                DatagramSocket ds = new DatagramSocket();
+                // String str =  // remove CRC8
+                //System.out.println(new String(rec, 0, paketti.getLength() - 1));
+                //System.out.println(rec[paketti.getLength() - 1]);
+                DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), 55105);
+                soketti.send(dp);
+
+
+            } catch (Exception e) {
+                listening = false;
+                System.out.println("catched");
+                break;
+
+
+                //DatagramSocket ulkoSoketti = null;
+
+                // ulkoSoketti = new VirtualSocket(54331);
+
+                //DatagramSocket ds = new DatagramSocket();
+                //  String str = "Welcome java ";
+                //InetAddress ip = InetAddress.getByName("127.0.0.1");
+
+                //DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ulkoSoketti, 54331);
+                //   ds.send(dp);
+                // ds.close();
+
+            }
+        }
+    }
+}
--- src/TestiSovellus.java
+++ src/TestiSovellus.java
@@ -1,13 +1,29 @@
 import java.io.*;
 import java.net.*;
-import java.net.Socket.*;
 import java.nio.charset.StandardCharsets;
 
 public class TestiSovellus {
-  //**  private static DatagramSocket soketti = null;
-    //private static DatagramSocket ulkoSoketti = null;
 
+    private static DatagramSocket soketti = null;
     public static void main(String[] args) throws Exception {
+        soketti  = new VirtualSocket(6666);
+        boolean listening = true;
+        while (listening) {
+            try {
+                byte[] rec = new byte[256];
+                DatagramPacket paketti = new DatagramPacket(rec, rec.length);
+                soketti.receive(paketti);
+                // String str =  // remove CRC8
+                System.out.println(new String(rec, 0, paketti.getLength() - 1));
+                System.out.println(rec[paketti.getLength() - 1]);
+            } catch (Exception e) {
+                listening = false;
+                System.out.println("catcheed");
+                break;
+            }
+        }
+    }
+}
 
     /**    ulkoSoketti = new VirtualSocket(54331);
 
@@ -21,16 +37,16 @@ public class TestiSovellus {
         System.out.println("Closing socket and terminating program.");
         socket.close();
 **/
-
+/**
             DatagramSocket ds = new DatagramSocket();
-            String str = "Welcome java ";
+            String str = "Welcome java";
             InetAddress ip = InetAddress.getByName("127.0.0.1");
 
-            DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ip, 54331);
+            DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ip, 3000);
             ds.send(dp);
-            ds.close();
-        }
-    }
+            ds.close();**/
+
+
 
 
 
