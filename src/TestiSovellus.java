ef58c3061f2b66b14f69236893600b6df8f9d375
esakesti <esakesti@gmail.com>
2021-11-28 00:39:32 +0000

    Testisovellus is sending data to the server without any issues, need to implement inheritance to datagramsocket.

--- src/TestiSovellus.java
+++ src/TestiSovellus.java
@@ -8,7 +8,7 @@ public class TestiSovellus {
    // private static DatagramSocket soketti = null;
     private static DatagramSocket ulkoSoketti = null;
 
-    public static void main(String[] args) throws SocketException {
+    public static void main(String[] args) throws IOException {
         /**soketti = new VirtualSocket(6666);
         boolean listening = true;
         while (listening) {
@@ -28,22 +28,12 @@ public class TestiSovellus {
                 break;
             }
         }**/
-        boolean kokeile = true;
-        while (kokeile) {
-            try {
-                byte[] bitti = new byte[256];
-                ulkoSoketti = new VirtualSocket();
-                String strn = "Testi";
-                InetAddress ip = InetAddress.getByName("127.0.0.1");
-                DatagramPacket paketti = new DatagramPacket(strn.getBytes(), strn.length(), bitti.length,ip,54331);
-                ulkoSoketti.send(paketti);
-                ulkoSoketti.close();
-            }catch (Exception e){
-                kokeile = false;
-                System.out.println("failed");
-                break;
-            }
-        }
 
+        DatagramSocket datasoc = new DatagramSocket();
+        String strn = "Welcome to DatagramSocket class";
+        InetAddress ip = InetAddress.getByName("127.0.0.1");
+        DatagramPacket dpac = new DatagramPacket(strn.getBytes(), strn.length(), ip, 65402);
+        datasoc.send(dpac);
+        datasoc.close();
     }
-        }
+}
--- src/VirtualSocket.java
+++ src/VirtualSocket.java
@@ -6,12 +6,19 @@ public class VirtualSocket extends DatagramSocket {
     private static double p_drop = 0.5;
 
     public VirtualSocket() throws SocketException {
-        super();
     }
 
     public VirtualSocket(int portti) throws SocketException {
         super(portti);
 
+    }
+
+    public void send(DatagramPacket paketti) throws IOException {
+        while(true) {
+            super.send(paketti);
+            System.out.println("Packet sent!");
+
+    }
     }
 
     public void receive(DatagramPacket packet) throws IOException {
